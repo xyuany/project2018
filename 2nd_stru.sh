9b6a75630abd27d547a4d5fd9dbd1a43d6ddbd43
xyuany <yyuanxi12@outlook.com>
2018-02-19 23:16:27 +0100

    add file structure

--- 2nd_stru.sh
+++ 2nd_stru.sh
@@ -1,3 +1,11 @@
-qwieuyqiwueyiq
-hifh asoidjhqwelkncsd
-33333333333333333333
+mkdir ./2nd_stru
+mkdir ./2nd_stru/scripts
+mkdir ./2nd_stru/bash
+mkdir ./2nd_stru/input
+mkdir ./2nd_stru/output
+mkdir ./2nd_stru/logs
+
+mkdir ./datasets
+mkdir ./general_scripts
+mkdir ./bin
+
