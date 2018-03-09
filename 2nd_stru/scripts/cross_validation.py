052d1ad2ca746c3537d3ae135036113fc09b2e6f
xyuany <yyuanxi12@outlook.com>
2018-03-09 02:09:50 +0100

    update parameter selection automatically

--- 2nd_stru/scripts/cross_validation.py
+++ 2nd_stru/scripts/cross_validation.py
@@ -1,8 +1,11 @@
 import numpy as np
 from sklearn import svm
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 import sys
 import StruPre as sp
 import training_testing_set as tt
+import timeit
 
 inputfile = sys.argv[1]
 
@@ -52,7 +55,7 @@ def cross_val(seq,topo,model):
 		score = model.score(test_seq,test_topo)
 		score_array = np.append(score_array, score)
 		#print (score)
-	mean = score_array.mean()
+	mean = np.average(score_array)
 	#print (score_array)
 	#print (mean)
 	return mean
@@ -79,9 +82,19 @@ for i in range(5):
 	print (group[i],train_seq,train_topo)	
 '''
 
+svmhandle = open ("./output/svm.txt",'w')
+kernal = ['linear','poly','rbf','sigmoid']
+svmhandle.write("win_size\tlinear(C=0.01)\tlinear(C=1)\tlinear(C=100)\tpoly(C=0.01)\tpoly(C=1)\tpoly(C=100)\trbf(C=0.01)\trbf(C=1)\trbf(C=100)\tsigmoid(C=0.01)\tsigmoid(C=1)\tsigmoid(C=100)\t\n")
 
+rfchandle = open("./output/rfc.txt",'w')
+rfchandle.write("win_size\testimator=10(mss=2)\testimator=10(mss=4)\testimator=10(mss=6)\testimator=20(mss=2)\testimator=20(mss=4)\testimator=20(mss=6)\testimator=30(mss=2)\testimator=30(mss=4)\testimator=30(mss=6)\testimator=40(mss=2)\testimator=40(mss=4)\testimator=40(mss=6)\testimator=50(mss=2)\testimator=50(mss=4)\testimator=50(mss=6)\t\n")
 
-for win_size in range(5,7,2):
+dtchandle = open("./output/dmc.txt",'w')
+dtchandle.write("win_size\tauto(mss=2)\tauto(mss=4)\tauto(mss=6)\tNone(mss=2)\tNone(mss=4)\tNone(mss=6)\t\n")
+
+timehandle = open("./output/time.txt",'w')
+
+for win_size in range(5,20,2):
 	tt.main(inputfile, win_size)
 	seq = dict()
 	topo = dict()
@@ -89,5 +102,41 @@ for win_size in range(5,7,2):
 	for i in range(1,6):
 		seq[group[i-1]], topo[group[i-1]] = load(i)
 	#for para in para_list:
-	clf = svm.SVC()
-	score = cross_val(seq,topo,clf)
+	#following is svm_training
+	svmhandle.write(str(win_size)+"\t")
+	for ker in kernal:
+		svm_start = timeit.default_timer()
+		for C in [0.01,1,100]:
+			clf = svm.SVC(C = C, cache_size=2000, kernel = ker)
+			score = cross_val(seq,topo,clf)
+			svmhandle.write(str(score)+"\t")
+		svm_end = timeit.default_timer()
+		timehandle.write("svm\t"+str(win_size)+"\t"+ker+"\t"+str(svm_end-svm_start)+"\n")
+	svmhandle.write("\n")
+	#following is random forest training
+	rfchandle.write(str(win_size)+"\t")
+	for estimator in [10,20,30,40,50]:
+		rf_start = timeit.default_timer()
+		for mss in [2,4,6]:
+			rfc = RandomForestClassifier(n_estimators = estimator, min_samples_split = mss)
+			score = cross_val(seq,topo,rfc)
+			rfchandle.write(str(score)+"\t")
+		rf_end = timeit.default_timer()
+		timehandle.write("rf\t"+str(win_size)+"\t"+str(estimator)+"\t"+str(rf_end-rf_start)+"\n")
+	rfchandle.write("\n")
+	#following is decision making trees
+	dtchandle.write(str(win_size)+"\t")
+	for mf in ["auto",None]:
+		dm_start = timeit.default_timer()
+		for mss in [2,4,6]:
+			dtc = DecisionTreeClassifier(max_features = mf, min_samples_split = mss)
+			score = cross_val(seq,topo,dtc)
+			dtchandle.write(str(score)+"\t")
+		dm_end = timeit.default_timer()
+		timehandle.write("dm\t"+str(win_size)+"\t"+str(mf)+"\t"+str(dm_end-dm_start)+"\n")
+	dtchandle.write("\n")
+
+svmhandle.close()
+rfchandle.close()
+dtchandle.close()
+timehandle.close()
