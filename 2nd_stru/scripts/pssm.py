23b9f41b5588e2a86e28174f8042c67e6946ee5b
xyuany <yyuanxi12@outlook.com>
2018-03-12 20:25:15 +0100

    main function of pssm, including sliding window

--- 2nd_stru/scripts/pssm.py
+++ 2nd_stru/scripts/pssm.py
@@ -1,34 +1,80 @@
 import os
 import numpy as np
 import sys
+import StruPre as sp
 
-
+#################################################
+#####You can change pssm files' path here
+################################################
 path = "./logs/pssm/"
 file_list = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path,f))] 
-
 #print (len(file_list))
+
+
+
+#####################################################################
+###Extract pssm and normalize it from files
+#####################################################################
 
 def parse_pssm(file_list):
 	pssm = dict()
-	for f in ['>154l-1-AUTO.1.pssm']:
+	for f in ['>154l-1-AUTO.1.pssm','>1aazb-1-DOMAK.pssm']:
 		#print (f)
 		file_array = np.genfromtxt(path+f, skip_header = 3, skip_footer = 5, usecols =range(22,42))
 		file_array = file_array/100
-		#print (file_array)
+		#print (file_array.shape)
 		key = f.lstrip('>').rstrip('.pssm')
 		pssm[key] = file_array
 		#print (pssm)
-		return pssm
+	return pssm
 
 
+def pssm_window(ID_list, pssm_array,win_size):
+	position = win_size//2
+	X = np.empty((0,win_size*20))
+	#print (X.shape)
+	for key in ID_list:
+		#print (key)
+		pssm_seq = pssm_array[key]
+		#print (pssm_seq.shape)
+		for i in range(len(pssm_seq)):
+			#print (len(pssm_seq))
+			prefix = i - position
+			suffix = i + position + 1 - len(pssm_seq)
+			if prefix <0:
+				zero = np.zeros(20*abs(prefix))
+				win_array = np.append(zero,pssm_seq[i+position-prefix-win_size+1:i+position+1,])
+				#print(win_array.shape)
+			elif suffix >0:
+				zero = np.zeros(20*abs(suffix))
+				win_array = np.append(pssm_seq[i-position:,],zero)
+				#print(win_array.shape)
+			else:
+				win_array = pssm_seq[i-position:i+position+1].flatten()
+			win_array = win_array.reshape(1,win_size*20)
+			X = np.append(X, win_array, axis = 0)
+			#print (win_array.shape)
+		#print (X.shape)
+	return X	
+
+inputfile = sys.argv[1]
+win_size = int(sys.argv[2])
+pssm = parse_pssm(file_list)
+#print (pssm)
+seq, topo = sp.data_stru(inputfile)
 
 
+def main(pssm,topo,win_size):
+	ID_list = sorted(pssm.keys())
+	X = pssm_window(ID_list, pssm, win_size)
+	#print (X.shape)
+	topo_encode_table = sp.uniq_topo(topo)
+	topo_num = sp.topo_encoded(topo,topo_encode_table)
+	Y = sp.topo_sliding_window(ID_list,topo_num)
+	#print (Y.shape)
+	return X,Y
 
-
-def main():
-	pssm = parse_pssm(file_list)
-	
-		
 if __name__ == '__main__':
+	main(pssm,topo,win_size)
 	
 
